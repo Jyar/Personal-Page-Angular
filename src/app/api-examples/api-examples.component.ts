ae426de63124c0620ed528c20f35c3b0cd5bdfbb
Jyar <jyar27@yahoo.com>
2022-02-23 23:54:23 -0500

    add cat facts

--- src/app/api-examples/api-examples.component.ts
+++ src/app/api-examples/api-examples.component.ts
@@ -1,7 +1,9 @@
 import { Component, ViewChild } from '@angular/core';
 import {MatAccordion} from '@angular/material/expansion';
+import { from, Observable } from 'rxjs';
 import {MatExpansionPanel} from '@angular/material/expansion'
-
+import { CatFact } from '../models/catFacts';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-api-examples',
@@ -9,7 +11,56 @@ import {MatExpansionPanel} from '@angular/material/expansion'
   styleUrls: ['./api-examples.component.scss']
 })
 export class ApiExamplesComponent{
-
+  books: any[];
+  catData: any;
+  // catData: Observable<CatFact>;
   @ViewChild(MatAccordion) accordion!: MatAccordion;
+
+  constructor(private http: HttpClient){
+    this.books = [
+      {
+        "id": 1,
+        "title": "Super Man"
+      },
+      {
+        "id": 2,
+        "title": "Spider Man"
+      },
+      {
+        "id": 3,
+        "title": "Aladdin"
+      }, 
+      {
+        "id": 4,
+        "title": "Downton Abbey"
+      }
+    ];
+
+    this.getCatFacts().subscribe(data => {
+      console.log(
+       this.catData = JSON.parse(data).fact
+        );
+    });
+  }
+
+  getCatFacts(){
+      const requestOptions: Object = {
+        observe: "body",
+        responseType: "text"
+      };
+      return this.http
+        .get<any>('https://catfact.ninja/fact', requestOptions);
+      
+  }
+
+  trackByBookCode(index: number, book: any): string {
+    return book.title;
+  }
+
+  ngOnInit(){
+
+  }
+
+ 
 
 }
--- src/app/api-examples/api-examples.module.ts
+++ src/app/api-examples/api-examples.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { ApiExamplesComponent } from './api-examples.component';
+import { CommonModule } from "@angular/common";
 
 @NgModule({
   declarations:[ApiExamplesComponent],
   imports: [
-    MatExpansionModule
+    MatExpansionModule,
+    CommonModule
   ],
   exports: [
     ApiExamplesComponent
