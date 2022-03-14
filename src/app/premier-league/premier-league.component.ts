d728250ee42bde7aefce773877b0ffad484fe621
Jyar <jyar27@yahoo.com>
2022-03-13 22:29:07 -0400

    import lodash and rxjs

--- src/app/premier-league/premier-league.component.ts
+++ src/app/premier-league/premier-league.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as CryptoJS from 'crypto-js';
+import * as _ from 'lodash';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-premier-league',
@@ -9,11 +12,16 @@ import * as CryptoJS from 'crypto-js';
 export class PremierLeagueComponent implements OnInit {
 private encryptedMessage: string = '';
 private decryptedMessage: string = '';
+numbers: any = '';
 
   constructor() { }
 
   ngOnInit(): void {
     this.encrypt();
+    of('1', '2', '3').pipe(map(x => ' next number: ' + x)).subscribe(result => {
+      this.numbers = result;
+      console.log(this.numbers)
+    }); // etc
   }
 
   encrypt() {
